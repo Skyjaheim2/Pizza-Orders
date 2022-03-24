4993fb905cab67c96cb56f33bcdf911b59ad78fc
Jaheim Archibald <jaheimarchibald@gmail.com>
2022-03-24 10:15:30 -0400

    Added sign in and sign up UI

---
+++ pizza/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.urls import reverse
+
+# Create your views here.
+
+def index(request):
+    return render(request, "pizza/index.html")
+
